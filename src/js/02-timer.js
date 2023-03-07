a9c2e0c80c17d9b16887ebeeba8a1427219f862a
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-03-07 15:50:14 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,18 @@
-// Описан в документации
 import flatpickr from 'flatpickr';
-// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('button[data-start]');
+const inputDate = document.querySelector('input[type="text"]');
+console.log(inputDate);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(inputDate, options);
